b359e37312d71cc2a043f3609bab8a8aa4fcae31
Dandamcfp <dandamcfp@gmail.com>
2025-09-23 17:20:05 -0300

    Mexendo um pouco nas cores

--- FRONTEND/src/theme/theme.js
+++ FRONTEND/src/theme/theme.js
@@ -3,14 +3,14 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
-      light: '#42a5f5',
-      dark: '#1565c0',
+      main: '#FFD700',
+      light: '#FAF0E6',
+      dark: '#6E6E6E',
     },
     secondary: {
-      main: '#dc004e',
-      light: '#ff5983',
-      dark: '#9a0036',
+      main: '#98FF98',
+      light: '#D4FFD4',
+      dark: '#4CAF50',
     },
   },
   typography: {
